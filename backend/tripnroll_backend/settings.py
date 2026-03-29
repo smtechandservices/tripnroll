1399bf84880dd7973846bdfdce70509c48370b1a
moon <pruthimayank2003@gmail.com>
2026-03-30 00:17:21 +0400

    prod fixes

--- backend/tripnroll_backend/settings.py
+++ backend/tripnroll_backend/settings.py
@@ -125,6 +125,11 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # CORS Configuration
 CORS_ALLOW_ALL_ORIGINS = os.getenv('CORS_ALLOW_ALL_ORIGINS', 'False') == 'True'
 CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', 'http://localhost:3000').split(',')
+CORS_ALLOW_CREDENTIALS = True
+CORS_EXPOSE_HEADERS = ["Content-Type", "X-CSRFToken"]
+
+# CSRF Configuration
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', 'http://localhost:3000').split(',')
 
 # REST Framework Configuration
 REST_FRAMEWORK = {
